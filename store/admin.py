b3cda1e4669f28335ca7991e15d5c7c4bbd6a7ad
admit-me <immrpuneeth@gmail.com>
2026-09-10 23:53:52 +0530

    Register e-commerce models in admin

---
+++ store/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import CartItem, Category, Order, OrderItem, Product
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
